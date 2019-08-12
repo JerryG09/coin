f3d9f9b48db3390a9620c2987059af0f6a78cd73
Victor CHINEWUBEZE <victor.chinewubeze@gmail.com>
2019-08-12 13:10:46 +0100

    setup api connection

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/common/Header';
-import './index.css'
+import './index.css';
+import List from './components/list/List'
 
 const App = () => {
-  const title = 'React Coin'
-
   return (
     <div>
       <Header />
-      <h1>{title}</h1>
+      <List />
 
       <p>Up to date crypto currencies financial data</p>
     </div>
